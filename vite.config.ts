6870735f1f13323b34e23c09571ecfdc60dd38ae
tolumide-ng <tolumideshopein@gmail.com>
2025-03-06 23:17:54 +0100

    Dynamically loads api_url with fallback

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import path from 'path';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
   resolve: {
@@ -14,6 +13,6 @@ export default defineConfig({
   server: {
     host: '0.0.0.0',
     port: 5173,
-    allowedHosts: ['blogger-l5fv.onrender.com']
+    allowedHosts: ['blogger-l5fv.onrender.com'],
   },
 });
